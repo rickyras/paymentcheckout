97465e6db419b40e5f4e048fc8cc6edb2533e77b
rickyras <ritchieperera@gmail.com>
2021-05-22 19:15:42 -0400

    http domain

--- create-checkout-session.php
+++ create-checkout-session.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 
 header('Content-Type: application/json');
 
-$YOUR_DOMAIN = 'https://rickyras.github.io/paymentcheckout';
+$YOUR_DOMAIN = 'http://rickyras.github.io/paymentcheckout';
 
 $checkout_session = \Stripe\Checkout\Session::create([
   'payment_method_types' => ['card'],
