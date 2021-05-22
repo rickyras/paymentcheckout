1549904a98aa74c0f221db99ee739d5771e75d52
rickyras <ritchieperera@gmail.com>
2021-05-22 18:44:25 -0400

    initial commit

---
+++ create-checkout-session.php
@@ -0,0 +1,28 @@
+<?php
+// hello
+require 'vendor/autoload.php';
+\Stripe\Stripe::setApiKey('sk_test_51ItfJ9Dt89XNbtXpWR3NDgTahaxHyi7mexIFWrN4Tu3xIul0netThqm4pN5ZBy5pl1fTbOlpAbN6uKGJn5x64qti003vdOMla4');
+
+header('Content-Type: application/json');
+
+$YOUR_DOMAIN = 'http://localhost/checkout/';
+
+$checkout_session = \Stripe\Checkout\Session::create([
+  'payment_method_types' => ['card'],
+  'line_items' => [[
+    'price_data' => [
+      'currency' => 'usd',
+      'unit_amount' => 2000,
+      'product_data' => [
+        'name' => 'Stubborn Attachments',
+        'images' => ["https://i.imgur.com/EHyR2nP.png"],
+      ],
+    ],
+    'quantity' => 1,
+  ]],
+  'mode' => 'payment',
+  'success_url' => $YOUR_DOMAIN . '/success.html',
+  'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+]);
+
+echo json_encode(['id' => $checkout_session->id]);
